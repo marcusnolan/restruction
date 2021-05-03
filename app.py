6309288006e18cfcb6c620c82f641cfa7803605e
marcusnolan <marcusnolan130@gmail.com>
2021-05-03 22:10:32 +0000

    Profile page added

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def register():
         # put the newuser into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successfull! Welcome to Restruction")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -65,7 +66,8 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Welcome back, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
 
             else:
                 flash("Incorrect Username and/or Password")
@@ -76,6 +78,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
