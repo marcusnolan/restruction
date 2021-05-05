54d8978d753198b103e7715bd88fac1a361a4ab6
marcusnolan <marcusnolan130@gmail.com>
2021-05-05 00:26:15 +0000

    Form to add new items to database created

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_item")
+def add_item():
+    return render_template("add_items.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
     $('.collapsible').collapsible();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: "yyyy/mm/dd",
+        yearRange:3,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+        }
+    });
   });
