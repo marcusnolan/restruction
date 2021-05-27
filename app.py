5021de12571d6a4a67b734cef4c08d670a1acb65
marcusnolan <marcusnolan130@gmail.com>
2021-05-27 23:11:38 +0000

    Styling added for 404 and 500 error pages

--- app.py
+++ app.py
@@ -261,7 +261,8 @@ def edit_users(users_id):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-    
+
+
 @app.errorhandler(500)
 def something_wrong(error):
     return render_template('500.html'), 500
